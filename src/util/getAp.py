842cf829429be44dd5298806d30b16aa34f529f9
dingnigefei <zluo6@illinois.edu>
2015-11-14 03:18:43 -0800

    rearranged files; updated getAp

--- src/util/getAp.py
+++ src/util/getAp.py
@@ -1,23 +1,14 @@
 import numpy as np
 from sklearn.metrics import average_precision_score
 
-imageTypes = ['rgb']
-directory = '/scail/scratch/group/vision/hospital/'
-crop = True
-random = False
-baseline = False
+# parameters
+fileName = 'rgb_crop_19_21'
+directory = '/scail/scratch/group/vision/hospital/src/ap/'
+# end parameters
 
-labelsFile = directory + 'src/ap/'
-for i in range(0, len(imageTypes)):
-    labelsFile = labelsFile + imageTypes[i] + '_'
-if crop:
-    labelsFile = labelsFile + 'crop_'
-if random:
-    labelsFile = labelsFile + 'random_'
-if baseline:
-    labelsFile = labelsFile + 'baseline_'
+labelsFile = directory + fileName + '_'
+
 pfix = ['test_scores.txt', 'test_true.txt', 'train_scores.txt', 'train_true.txt']
-#pfix = ['train_scores.txt', 'train_true.txt']
 for i in range(0, 1):
     file1 = labelsFile + pfix[i*2]
     file2 = labelsFile + pfix[i*2+1]
