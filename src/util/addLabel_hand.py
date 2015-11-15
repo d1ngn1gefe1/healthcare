f8386553aebc6075e495a9604728cbe447b25ab3
Emma Peng <u3502350@connect.hku.hk>
2015-11-15 11:50:23 -0800

    Modify video display

--- src/util/addLabel_hand.py
+++ src/util/addLabel_hand.py
@@ -2,6 +2,9 @@
 from PIL import Image, ImageFont, ImageDraw
 import numpy as np
 
+imageType = 'd'
+depth = True
+
 # Read in labels
 txt = open('filtered_label.txt', 'r')
 labels = np.array([])
@@ -11,7 +14,7 @@ for line in txt:
 
 # Add text to each image
 font1 = ImageFont.truetype('/Library/Fonts/Arial.ttf', 10)
-font2 = ImageFont.truetype('/Library/Fonts/Arial.ttf', 20)
+font2 = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
 text1 = 'Threshold'
 text2 = 'Hand Hygiene!'
 
@@ -22,44 +25,30 @@ offset1 = 14769
 offset2 = 30645
 
 count = 0
-for i in range(0,num1):
-  index = 3*i + offset1
-  img = Image.open('d_19_21/d-' + str(index) + '.jpg')
+for i in range(0,num1+num2):
+  if i < num1:
+    index = 3*i + offset1
+  else:
+    index = 3*(i-num1) + offset2
+  img = Image.open(imageType + '_19_21/' + imageType + '-' + str(index) + '.jpg')
 
   draw = ImageDraw.Draw(img)
-  #draw.rectangle([(10, 10), (170, 70)], fill=(200, 200, 200, 100))
-  draw.line([(90, 15), (90, 30)], fill=(200, 0, 0))
-  draw.rectangle([(15, 15), (165, 30)], outline=(100, 100, 100))
   w1, h1 = draw.textsize(text1, font1)
-  draw.text((15+75-w1/2, 30), text1, font=font1, fill=(100, 0, 0))
+  draw.rectangle([(15, 15), (165, 30)], outline=(100, 100, 100))
+  if not depth:
+    draw.rectangle([(15, 15), (165, 30)], fill=(200, 200, 200))
+    draw.rectangle([(15, 2), (18+w1, 3+h1)], fill=(200, 200, 200))
+    draw.text((17, 2), text1, font=font1, fill=(0, 0, 0))
+  else:
+    draw.text((17, 2), text1, font=font1, fill=(255, 255, 255))
 
   if labels[count] == 1:
     print(count)
-    draw.rectangle([(15, 15), (165, 30)], fill=(100, 0, 0))
     w2, h2 = draw.textsize(text2, font2)
-    draw.text((15+75-w2/2, 35+h1), text2, font=font2, fill=(100, 0, 0))
+    if not depth:
+      draw.rectangle([(15+70-w2/2, 30+h1), (15+80+w2/2, 40+h1+h2)], fill=(200, 200, 200))
+    draw.rectangle([(15, 15), (165, 30)], fill=(204, 0, 0))
+    draw.text((15+75-w2/2, 35+h1), text2, font=font2, fill=(204, 0, 0))
     
-  img.save('d_hand_text_19_21/' + str(count) + '.jpg', 'JPEG', quality=90)
+  img.save(imageType + '_hand_text_19_21/' + str(count) + '.jpg', 'JPEG', quality=90)
   count += 1
-
-for i in range(0,num2):
-  index = 3*i + offset2
-  img = Image.open('d_19_21/d-' + str(index) + '.jpg')
-
-  draw = ImageDraw.Draw(img)
-  #draw.rectangle([(10, 10), (170, 70)], fill=(200, 200, 200, 100))
-  draw.line([(90, 15), (90, 30)], fill=(200, 0, 0))
-  draw.rectangle([(15, 15), (165, 30)], outline=(100, 100, 100))
-  w1, h1 = draw.textsize(text1, font1)
-  draw.text((15+75-w1/2, 30), text1, font=font1, fill=(100, 0, 0))
-
-  if labels[count] == 1:
-    print(count)
-    draw.rectangle([(15, 15), (165, 30)], fill=(100, 0, 0))
-    w2, h2 = draw.textsize(text2, font2)
-    draw.text((15+75-w2/2, 35+h1), text2, font=font2, fill=(100, 0, 0))    
-
-  img.save('d_hand_text_19_21/' + str(count) + '.jpg', 'JPEG', quality=90)
-  count += 1
-
-
