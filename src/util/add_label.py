b78a1458d4dc2ad77f835b4f4471fa976655a110
dingnigefei <zluo6@illinois.edu>
2015-11-15 15:38:33 -0800

    add_label modified; remove_frames supports deleting frames

--- src/util/add_label.py
+++ src/util/add_label.py
@@ -34,8 +34,8 @@ for line in testFile:
 
 assert (labels.shape[0] == prob.shape[0] == test.shape[0])
 
-font1 = ImageFont.truetype('/Library/Fonts/Arial.ttf', 10)
-font2 = ImageFont.truetype('/Library/Fonts/Arial.ttf', 20)
+font1 = ImageFont.truetype('/Library/Fonts/Arial.ttf', 11)
+font2 = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
 text1 = 'Threshold'
 text2 = 'Hand Hygiene!'
 
@@ -44,21 +44,25 @@ for i in range(0, num):
     index = 3*i
     if (depth):
         test[i] = test[i].replace('rgb', 'd')
-    img = Image.open(dir + test[i])
+        img = Image.open(dir + test[i]).convert('RGB')
+    else:
+        img = Image.open(dir + test[i]).convert('RGB')
     #print(dir + test[i])
     assert (img != None)
-    draw = ImageDraw.Draw(img)
+
+    draw = ImageDraw.Draw(img, 'RGBA')
     w1, h1 = draw.textsize(text1, font1)
-    if (not depth):
-        draw.rectangle([(15, 15), (165, 30)], fill=(200, 200, 200))
-    draw.rectangle([(15, 15), (15+150*prob[i], 30)], fill=(100, 0, 0) if (not depth) else 255)
-    draw.line([(90, 15), (90, 30)], fill=(200, 0, 0) if (not depth) else 255)
-    draw.rectangle([(15, 15), (165, 30)], outline=(100, 100, 100) if (not depth) else 255)
-    draw.text((15+75-w1/2, 30), text1, font=font1, fill=(100, 0, 0) if (not depth) else 255)
+    w2, h2 = draw.textsize(text2, font2)
+
+    draw.rectangle([(10, 5), (170, 30+h1+h2)], fill=(50, 50, 50, 200))
+    draw.text((15, 10), text1, font=font1, fill=(200, 200, 200))
+    
     if labels[i] == 1:
-        w2, h2 = draw.textsize(text2, font2)
-        if (not depth):
-            draw.rectangle([(15+75-w2/2, 35+h1), (15++75+w2/2, 35+h1+h2)], fill=(200, 200, 200))
-        draw.text((15+75-w2/2, 35+h1), text2, font=font2, fill=(100, 0, 0) if (not depth) else 255)
-    img.save(outDir + str(i) + '.jpg', 'JPEG', quality=90)
+        draw.rectangle([(15, 15+h1), (15+150*prob[i], 30+h1)], fill=(255, 0, 0))
+        draw.text((15+75-w2/2, 30+h1), text2, font=font2, fill=(255, 0, 0))
+    else:
+        draw.rectangle([(15, 15+h1), (15+150*prob[i], 30+h1)], fill=(200, 200, 200))
+
+    draw.rectangle([(15, 15+h1), (165, 30+h1)], outline=(100, 100, 100))
+    img.save(outDir + str(i) + '.png', 'PNG')
     #print(outDir + test[i].replace('/', '-'))
