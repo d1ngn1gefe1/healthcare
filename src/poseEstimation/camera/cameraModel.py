b1333d1f117388c0ea8b91bfa82507bd057e762e
Emma Peng <u3502350@connect.hku.hk>
2016-01-06 20:55:25 -0800

    General rotation matrix

--- src/poseEstimation/camera/cameraModel.py
+++ src/poseEstimation/camera/cameraModel.py
@@ -1,27 +1,19 @@
 import numpy as np
 import math
 
-root2 = math.sqrt(2)
-d1 = 2e3
-d2 = 2.7e3
+def rigidBodyMotion(coord,dx,dy,dz,rx,ry,rz):
+    rotateX = np.array([[1,0,0,0],[0,math.cos(rx),-math.sin(rx),0],[0,math.sin(rx),math.cos(rx),0],[0,0,0,1]])
+    rotateY = np.array([[math.cos(ry),0,math.sin(ry),0],[0,1,0,0],[-math.sin(ry),0,math.cos(ry),0],[0,0,0,1]])
+    rotateZ = np.array([[math.cos(rz),-math.sin(rz),0,0],[math.sin(rz),math.cos(rz),0,0],[0,0,1,0],[0,0,0,1]])
+    
+    rotation = np.dot(np.dot(rotateZ,rotateY),rotateX)
+    #print(rotation)
+    
+    t = np.eye(4)
+    t[:,3] = np.array([dx,dy,dz,1])
 
-rotation = np.array([[-1,0,0],[0,0,-1],[0,-1,0]])
-t = np.array([[0],[d1],[d2]])
+    projection = np.dot(rotation,t)
 
-translation = -np.dot(rotation,t)
-projection = np.zeros((4,4))
-
-for i in range(0,3):
-    for j in range(0,3):
-        projection[i][j] = rotation[i][j]
-
-for i in range(0,3):
-    projection[i][3] = translation[i]
-
-projection[3][3] = 1
-
-# e.g. coord0 = np.random.rand(3,1)
-def rigidBodyMotion(coord):
     homoCord = np.ones((4,1))
     for i in range(0,3):
         homoCord[i] = coord[i]
--- src/poseEstimation/camera/projection.py
+++ src/poseEstimation/camera/projection.py
@@ -1,12 +1,19 @@
 import numpy as np
 import cameraModel as cm
 
+dx = 0
+dy = 2e3
+dz = 2.7e3
+rx = math.radians(180)
+ry = math.radians(45)
+rz = math.radians(235)
+
 frontJoints = np.loadtxt('joints.dat',delimiter=',')
 num = frontJoints.shape[0]
 
 topJoints = np.zeros((num,3))
 for i in range(0,num):
-    topJoints[i] = (cm.rigidBodyMotion(frontJoints[i])).reshape(3)
+    topJoints[i] = (cm.rigidBodyMotion(frontJoints[i],dx,dy,dz,rx,ry,rz)).reshape(3)
 
 np.savetxt('topJoints.txt',topJoints,delimiter=',',newline='\n')
 
