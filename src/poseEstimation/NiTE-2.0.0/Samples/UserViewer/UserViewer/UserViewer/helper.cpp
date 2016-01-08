3f1b3f4f3a040c281d75651d38f6b0fba5177663
dingnigefei <zluo6@illinois.edu>
2016-01-07 20:51:33 -0800

    helper

---
+++ src/poseEstimation/NiTE-2.0.0/Samples/UserViewer/UserViewer/UserViewer/helper.cpp
@@ -0,0 +1,13 @@
+//
+//  helper.cpp
+//  UserViewer
+//
+//  Created by Zelun Luo on 1/7/16.
+//  Copyright © 2016 Zelun Luo. All rights reserved.
+//
+
+#include "helper.hpp"
+
+void side2top(float side[][5], float top[][5]) {
+    
+}
---
+++ src/poseEstimation/NiTE-2.0.0/Samples/UserViewer/UserViewer/UserViewer/helper.hpp
@@ -0,0 +1,18 @@
+//
+//  helper.hpp
+//  UserViewer
+//
+//  Created by Zelun Luo on 1/7/16.
+//  Copyright © 2016 Zelun Luo. All rights reserved.
+//
+
+#ifndef helper_hpp
+#define helper_hpp
+
+#include <stdio.h>
+
+#define N_JOINTS 15
+
+void side2top(float side[][5], float top[][5]);
+
+#endif /* helper_hpp */
