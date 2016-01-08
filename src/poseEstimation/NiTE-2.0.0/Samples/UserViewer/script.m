7a729eacc95fece42f4f32d1b60f522cff76d450
dingnigefei <zluo6@illinois.edu>
2016-01-07 18:14:35 -0800

    visualization

--- src/poseEstimation/NiTE-2.0.0/Samples/UserViewer/script.m
+++ src/poseEstimation/NiTE-2.0.0/Samples/UserViewer/script.m
@@ -1,24 +1,43 @@
 function [] = script()
-	load label.dat;
-	load depth.dat;
-	load joints.dat;
-	load topJoints.dat;
+	%load label1.dat;
+	load depth2.dat;
+	load joints1.dat;
+    load topJoints2.dat;
+    %label = label1;
+    depth = depth2;
+    joints = joints1;
+	topJoints = topJoints2;
 
 	width = 320;
 	height = 240;
 
-	label = reshape(label, [width, height])';
-	label = mat2gray(label);
-	plot2d(label, joints(:, 4:5));
+	%label = reshape(label, [width, height])';
+	%label = mat2gray(label);
+	%plot2d(label, joints(:, 4:5));
 
 	depth = reshape(depth, [width, height])';
 	depth = mat2gray(depth);
 	plot2d(depth, joints(:, 4:5));
     
-    plot3d(joints(:, 1:3));
-    plot3d(topJoints);
-    plotProj(joints(:, 1:2));
-    plotProj(topJoints(:, 1:2));
+    plot3d(joints(:, 1:3), [-1000 1000 -1000 1000 2000 4000]);
+    plot3d(topJoints, [-1000 1000 -1000 1000 0 2000]);
+    %plotProj(joints(:, 1:2));
+    %plotProj(topJoints(:, 1:2));
+    
+    bx = getParam(joints(:, 1), joints(:, 4));
+    by = getParam(joints(:, 2), joints(:, 5));
+    [topJoints(:,4), topJoints(:,5)] = world2depth(bx, by, topJoints(:,1), topJoints(:,2))
+    plotProj([topJoints(:,4), topJoints(:,5)], [0 320 0 240]);
+end
+
+function [xd, yd] = world2depth(bx, by, xw, yw)
+    xd = bx(1) + bx(2)*xw;
+    yd = by(1) + by(2)*yw;
+end
+
+
+function [b] = getParam(x, y)
+    b = [ones(length(x), 1) x]\y;
 end
 
 function [] = plot2d(img, mat)
@@ -28,7 +47,7 @@ function [] = plot2d(img, mat)
     scatter(mat(:, 1), mat(:, 2));
 end
 
-function [] = plotProj(mat)
+function [] = plotProj(mat, axisMat)
 	figure;
 	% JOINT_HEAD
 	% JOINT_NECK 	
@@ -113,9 +132,10 @@ function [] = plotProj(mat)
 	xlabel('x');
 	ylabel('y');
     axis equal;
+    axis(axisMat);
 end
 
-function [] = plot3d(mat)
+function [] = plot3d(mat, axisMat)
 	figure;
 	% JOINT_HEAD
 	% JOINT_NECK 	
@@ -133,8 +153,12 @@ function [] = plot3d(mat)
 	% JOINT_LEFT_FOOT 	
 	% JOINT_RIGHT_FOOT 
 
-	pts = [mat(1,:); mat(2,:)];
+	pts = [mat(1,:); mat(2,:)]; % JOINT_HEAD, JOINT_NECK 
 	plot3(pts(:,1), pts(:,2), pts(:,3));
+    center = (mat(1,:) + mat(2,:)) ./ 2;
+    radius = pdist(pts, 'euclidean') / 2;
+    [x,y,z] = sphere; 
+    surf(x*radius+center(1),y*radius+center(2),z*radius+center(3));
 	hold on;
 
 	pts = [mat(2,:); mat(3,:)];
@@ -145,20 +169,20 @@ function [] = plot3d(mat)
 	plot3(pts(:,1), pts(:,2), pts(:,3));
 	hold on;
 
-	pts = [mat(5,:); mat(3,:)];
-	plot3(pts(:,1), pts(:,2), pts(:,3));
+	pts = [mat(5,:); mat(3,:)]; % JOINT_LEFT_SHOULDER, JOINT_LEFT_ELBOW
+	plot3(pts(:,1), pts(:,2), pts(:,3), 'LineWidth', 10);
 	hold on;
 
-	pts = [mat(6,:); mat(4,:)];
-	plot3(pts(:,1), pts(:,2), pts(:,3));
+	pts = [mat(6,:); mat(4,:)]; % JOINT_RIGHT_SHOULDER, JOINT_RIGHT_ELBOW
+	plot3(pts(:,1), pts(:,2), pts(:,3), 'LineWidth', 10);
 	hold on;
 
 	pts = [mat(5,:); mat(7,:)];
-	plot3(pts(:,1), pts(:,2), pts(:,3));
+	plot3(pts(:,1), pts(:,2), pts(:,3), 'LineWidth', 10);
 	hold on;
 
 	pts = [mat(6,:); mat(8,:)];
-	plot3(pts(:,1), pts(:,2), pts(:,3));
+	plot3(pts(:,1), pts(:,2), pts(:,3), 'LineWidth', 10);
 	hold on;
 
 	pts = [mat(9,:); mat(3,:)];
@@ -182,23 +206,38 @@ function [] = plot3d(mat)
 	hold on;
 
 	pts = [mat(12,:); mat(10,:)];
-	plot3(pts(:,1), pts(:,2), pts(:,3));
+	plot3(pts(:,1), pts(:,2), pts(:,3), 'LineWidth', 20);
 	hold on;
 
 	pts = [mat(13,:); mat(11,:)];
-	plot3(pts(:,1), pts(:,2), pts(:,3));
+	plot3(pts(:,1), pts(:,2), pts(:,3), 'LineWidth', 20);
 	hold on;
 
 	pts = [mat(12,:); mat(14,:)];
-	plot3(pts(:,1), pts(:,2), pts(:,3));
+	plot3(pts(:,1), pts(:,2), pts(:,3), 'LineWidth', 20);
 	hold on;
 
 	pts = [mat(13,:); mat(15,:)];
-	plot3(pts(:,1), pts(:,2), pts(:,3));
+	plot3(pts(:,1), pts(:,2), pts(:,3), 'LineWidth', 20);
 	hold on;
 
-	xlabel('x');
+    mat(13,:); mat(15,:)
+    pt1 = (mat(3,:) + mat(4,:)) ./ 2;
+    pt2 = (mat(3,:) + pt1) ./ 2;
+    pt3 = (mat(4,:) + pt1) ./ 2;
+    pt4 = (mat(10,:) + mat(11,:)) ./ 2;
+    pt5 = (mat(10,:) + pt4) ./ 2;
+    pt6 = (mat(11,:) + pt4) ./ 2;
+    width = pdist([pt1; pt3], 'euclidean') / 2;
+    
+    pts = [pt2; pt5];
+    %plot3(pts(:,1), pts(:,2), pts(:,3), 'LineWidth', width);  
+    pts = [pt3; pt6];
+    %plot3(pts(:,1), pts(:,2), pts(:,3), 'LineWidth', width);
+    
+    xlabel('x');
 	ylabel('y');
 	zlabel('z');
-    axis equal;
+    axis(axisMat);
+    view([0,0,1]);
 end
