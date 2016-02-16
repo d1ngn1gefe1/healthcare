f3eb0a72a89f97f63b33bfb3bc2018ef2e39bb87
Alan <luozl1993@gmail.com>
2016-02-15 23:20:39 -0800

    proc_depth processes joint locations

--- src/util/proc_depth.py
+++ src/util/proc_depth.py
@@ -6,21 +6,49 @@ import cv2
 W = 320
 H = 240
 datasets = glob.glob('/mnt0/data/EVAL/data/*')
-print datasets
+#print datasets
 
 C = 3.8605e-3 #NUI_CAMERA_DEPTH_NOMINAL_INVERSE_FOCAL_LENGTH_IN_PIXELS
 
-
 for dataset in datasets:
-	inDir = dataset + '/depth/'
-	outDir = dataset + '/depth_frames/' 
+	inImgsDir = dataset + '/depth/'
+	inJointsDir = dataset + '/joints/'
+	outImgsDir = dataset + '/imgs_depthcoor/'
+	outJointsDir = dataset + '/joints_depthcoor/' 
 
-	if not os.path.exists(outDir):
-	    	os.makedirs(outDir)
+	if not os.path.exists(outImgsDir):
+		os.makedirs(outImgsDir)
+	if not os.path.exists(outJointsDir):
+		os.makedirs(outJointsDir)
 
-	paths = glob.glob(inDir + '*.txt')
+	paths = glob.glob(inJointsDir + '*.txt')
 	for i, path in enumerate(paths):
-		fName = path.replace(inDir, '').replace('txt', 'jpg')
+		fName = path.replace(inJointsDir, '')
+		if i%100 == 0:
+			print 'joint %d: %s' % (i+1, fName)
+
+		f = np.loadtxt(path)
+		if f.shape[0] == 0:
+			continue
+
+		worldX = f[:, 0]
+		worldY = f[:, 1]
+		worldZ = f[:, 2]
+
+		depthZ = worldZ
+		depthX = worldX/worldZ/C + W/2.0
+		depthY = worldY/worldZ/C + H/2.0
+
+		depthX = np.clip(np.rint(depthX), 0, W-1).astype(np.int32)
+		depthY = np.clip(np.rint(depthY), 0, H-1).astype(np.int32)
+
+		out = np.column_stack((depthX, depthY, depthZ))
+		np.savetxt(outJointsDir + fName, out)
+
+'''
+	paths = glob.glob(inImgsDir + '*.txt')
+	for i, path in enumerate(paths):
+		fName = path.replace(inImgsDir, '').replace('txt', 'jpg')
 		if i%100 == 0:
 			print 'image %d: %s' % (i+1, fName)
 
@@ -30,7 +58,7 @@ for dataset in datasets:
 		indices = np.nonzero(f[:, 2])
 
 		if len(indices[0]) == 0:
-			cv2.imwrite(outDir + fName, out)
+			cv2.imwrite(outImgsDir + fName, out)
 			continue
 
 		worldX = f[:, 0][indices]
@@ -40,27 +68,28 @@ for dataset in datasets:
 		#print np.amin(worldY), np.amax(worldY)
 		#print np.amin(worldZ), np.amax(worldZ)
 
-		depthZ = worldZ
+		depth = worldZ
 		depthX = worldX/worldZ/C + W/2.0
 		depthY = worldY/worldZ/C + H/2.0
 		#print np.amin(depthX), np.amax(depthX)
 		#print np.amin(depthY), np.amax(depthY)
-		#print np.amin(depthZ), np.amax(depthZ)
+		#print np.amin(depth), np.amax(depth)
 
 		depthX = np.clip(np.rint(depthX), 0, W-1).astype(np.int32)
 		depthY = np.clip(np.rint(depthY), 0, H-1).astype(np.int32)
-		zMin = np.amin(depthZ)
-		zMax = np.amax(depthZ)
-		depthZ = (depthZ-zMin)/(zMax-zMin)*255
+		zMin = np.amin(depth)
+		zMax = np.amax(depth)
+		depth = (depth-zMin)/(zMax-zMin)*255
 
 		#print np.amin(depthX), np.amax(depthX)
 		#print np.amin(depthY), np.amax(depthY)
-		#print np.amin(depthZ), np.amax(depthZ)
+		#print np.amin(depth), np.amax(depth)
 
-		out[depthY, depthX] = depthZ
+		out[depthY, depthX] = depth
 
 		out = cv2.equalizeHist(out.astype(np.uint8))
-		cv2.imwrite(outDir + fName, out)
+		cv2.imwrite(outImgsDir + fName, out)
 		#cv2.imshow('image', out)
 		#cv2.waitKey(0)
 		#cv2.destroyAllWindows()
+'''
