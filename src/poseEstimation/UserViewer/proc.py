b4204ff79bf0ca98b0b30f09e34c7e12ffc0cf91
dingnigefei <luozl1993@gmail.com>
2016-02-27 21:42:03 -0800

    process data ready

--- src/poseEstimation/UserViewer/proc.py
+++ src/poseEstimation/UserViewer/proc.py
@@ -43,6 +43,9 @@ jointName = ['HEAD', 'NECK', 'LEFT_SHOULDER', 'RIGHT_SHOULDER', \
 
 skeleton = [(0,1), (1,2), (1,3), (2,4), (3,5), (4,6), (5,7), (2,8), (3,8), \
             (8,9), (8,10), (9,10), (9,11), (10,12), (11,13), (12,14)]
+# first joint vs second joint
+relativeWeights = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.3, \
+                   0.3, 0.7, 0.7, 0.5, 0.5, 0.5, 0.5, 0.5]
 
 '''
   pixelZ = worldZ
@@ -71,22 +74,25 @@ def world2pixel(world, C):
   pixel[:, 1] = -world[:, 1]/world[:, 2]/C + H/2.0
   return pixel.astype(int)
 
-def visualizePts(pixel, label=None):
-  if label != None:
-    img = np.zeros((H, W, 3))
+def visualizePts(pixel, name, label=None):
+  if label is None:
+    img = np.zeros((H, W), np.uint8)
   else:
-    img = np.zeros((H, W))
+    img = np.zeros((H, W, 3), np.uint8)
 
-  print pixel.shape
-  print pixel
   for i, pt in enumerate(pixel):
-    if label != None:
-      #img[pt[1], pt[0]] = np.asarray(palette[label[i]])
-      cv2.circle(img, (pt[0], pt[1]), 4, palette[label[i]], -1)
-    else:
+    if label is None:
       img[pt[1], pt[0]] = pt[2]
-  cv2.imshow('visualize', img)
-  cv2.waitKey(0)
+    else:
+      #img[pt[1], pt[0]] = np.asarray(palette[label[i]])
+      cv2.circle(img, (pt[0], pt[1]), 1, palette[label[i]], -1)
+      #print label[i], palette[label[i]]
+    cv2.imshow(name, img)
+
+def drawID(img, id):
+  cv2.putText(img, str(id), (10, 10), \
+              cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255))
+  return img
 
 def drawJoints(img, joints, bad):
   # right bar
@@ -131,7 +137,7 @@ def joints2skeleton(joints):
   ptsAll = np.array([]).reshape(0, 3)
   labelsAll = np.array([]).reshape(0)
 
-  for jointPair in skeleton:
+  for i, jointPair in enumerate(skeleton):
     x, y, z, nPts = None, None, None, 0
     pt1 = joints[jointPair[0]]
     pt2 = joints[jointPair[1]]
@@ -139,51 +145,66 @@ def joints2skeleton(joints):
     if abs(pt1[0]-pt2[0]) < abs(pt1[1]-pt2[1]):
       start = min(pt1[1], pt2[1])
       end = max(pt1[1], pt2[1])
-      y = np.arange(start, end, (end-start)/20)
+      y = np.arange(start, end, (end-start)/20.0)
       x, z = y2xz(pt1, pt2, y)
       nPts = y.shape[0]
+      if nPts == 0:
+        continue
+      labels = np.empty(nPts)
+      nPart1 = int(relativeWeights[i]*nPts)
+      nPart2 = nPts - nPart1
+      nParts = [nPart1, nPart2]
+      idxSm = np.argmin([pt1[1], pt2[1]])
+      idxLg = np.argmax([pt1[1], pt2[1]])
+      labels[:nParts[idxSm]] = jointPair[idxSm]
+      labels[nParts[idxSm]:] = jointPair[idxLg]
     else:
-      start = min(pt1[0], pt1[0])
-      end = max(pt1[0], pt1[0])
-      x = np.arange(start, end, (end-start)/20)
+      start = min(pt1[0], pt2[0])
+      end = max(pt1[0], pt2[0])
+      x = np.arange(start, end, (end-start)/20.0)
       y, z = x2yz(pt1, pt2, x)
       nPts = x.shape[0]
+      if nPts == 0:
+        continue
+      labels = np.empty(nPts)
+      nPart1 = int(relativeWeights[i]*nPts)
+      nPart2 = nPts - nPart1
+      nParts = [nPart1, nPart2]
+      idxSm = np.argmin([pt1[0], pt2[0]])
+      idxLg = np.argmax([pt1[0], pt2[0]])
+      labels[:nParts[idxSm]] = jointPair[idxSm]
+      labels[nParts[idxSm]:] = jointPair[idxLg]
 
-    if nPts == 0:
-      continue
     pts = np.vstack((x, y, z)).T
     ptsAll = np.concatenate((ptsAll, pts))
     #print ptsAll.shape, pts.shape
-
-    labels = np.empty(nPts)
-    labels[:nPts/2] = jointPair[0]
-    labels[nPts/2:] = jointPair[1]
     labelsAll = np.concatenate((labelsAll, labels))
     #print labelsAll.shape, labels.shape
 
   return (ptsAll, labelsAll.astype(int))
 
-def knn(depth, joints, C):
+# zScale: how much we trust the z value. 1 indicates equal trust on x, y, z
+def knn(depth, joints, C, name=None, zScale=1.0):
   pts_world, labels = joints2skeleton(joints[:, :3])
-  visualizePts(world2pixel(pts_world, C), labels)
+  if name is not None:
+      visualizePts(world2pixel(pts_world, C), name, labels)
+  pts_world[:, 2] *= zScale
   classifier = KNeighborsClassifier(n_neighbors=5)
-  pts_world[:, 2] = 0
   classifier.fit(pts_world, labels)
 
   X = np.vstack((np.nonzero(depth)[1], np.nonzero(depth)[0]))
   X = np.vstack((X, depth[depth != 0]))
   X_world = pixel2world(X.T, C)
-  X_world[:, 2] = 0
+  X_world[:, 2] *= zScale
   predicts = classifier.predict(X_world)
 
   perPixelLabels = -np.ones(depth.shape)
   perPixelLabels[depth != 0] = predicts
 
-  img = np.zeros((H, W, 3))
+  img = np.zeros((H, W, 3), np.uint8)
   for i in range(nJoints):
     img[perPixelLabels == i] = palette[i]
 
-  cv2.imshow('kNNSide', img.astype(np.uint8))
   return img
 
 def main(**kwargs):
@@ -192,6 +213,8 @@ def main(**kwargs):
   deleleBad = True
   dataDir = kwargs.get('dir')
   id = kwargs.get('id')
+  startFrame = kwargs.get('start')
+  key = 0
 
   for i in range(0 if id == -1 else id, nPeople if id == -1 else id+1):
     person = dataDir+str(i).zfill(2)+'_'+'[0-9]'*5+'_'
@@ -207,10 +230,22 @@ def main(**kwargs):
     N = len(depthSideFiles)
     print 'person id: %d; #frames: %d' % (i, N)
 
-    for j in range(N):
-      curFrame = depthSideFiles[j].replace(dataDir, '')\
-                   .replace('_depth_side.txt', '').replace(str(i)+'_', '')
-      print 'current frame: %s' % curFrame
+    j = 0
+    while j < N:
+      curFrame = int(depthSideFiles[j].replace(dataDir, '')\
+                   .replace('_depth_side.txt', '').replace(str(i)+'_', ''))
+      print 'current frame: %d' % curFrame
+      if curFrame < startFrame:
+        j += 1
+        continue
+      if not os.path.exists(depthSideFiles[j]):
+        print 'file not exist: %d' % curFrame
+        if key == 65361:
+          j -= 1
+          continue
+        else:
+          j += 1
+          continue
 
       depthSide = np.loadtxt(depthSideFiles[j], dtype=float).reshape((H, W))
       depthTop = np.loadtxt(depthTopFiles[j], dtype=float).reshape((H, W))
@@ -223,7 +258,7 @@ def main(**kwargs):
         np.count_nonzero(jointsSide) == 0:
         bad = True
         if deleleBad:
-            print 'automatically delete bad frame %s' % curFrame
+            print 'automatically delete bad frame %d' % curFrame
             os.remove(depthSideFiles[j])
             os.remove(depthTopFiles[j])
             os.remove(jointsSideFiles[j])
@@ -238,7 +273,9 @@ def main(**kwargs):
         C = getC(jointsSide)
 
       # make and apply masks
-      labelSide[labelSide != 0] = 1
+      mostFreqLabel = np.argmax(np.bincount(labelSide.ravel())[1:])+1
+      labelSide[labelSide != mostFreqLabel] = 0
+      labelSide[labelSide == mostFreqLabel] = 1
       labelTop[labelTop != -1] = 1
       labelTop[labelTop == -1] = 0
       depthSide *= labelSide
@@ -248,22 +285,27 @@ def main(**kwargs):
       dispSide = cv2.applyColorMap(dispSide, cv2.COLORMAP_SPRING)
       dispSide = np.multiply(dispSide, labelSide[:, :, np.newaxis])
       dispSide = drawJoints(dispSide, jointsSide, bad)
+      dispSide = drawID(dispSide, curFrame)
       cv2.imshow('depthSide', dispSide)
 
       dispTop = cv2.equalizeHist(depthTop.astype(np.uint8))
       dispTop = cv2.applyColorMap(dispTop, cv2.COLORMAP_SPRING)
       dispTop = np.multiply(dispTop, labelTop[:, :, np.newaxis])
       dispTop = drawJoints(dispTop, jointsTop, bad)
+      dispTop = drawID(dispTop, curFrame)
       cv2.imshow('depthTop', dispTop)
 
       # knn
-      #if not bad:
-      #  img = knn(depthSide, jointsSide, C)
+      perPixelSide = knn(depthSide, jointsSide, C, 'sideKNN')
+      perPixelTop = knn(depthTop, jointsTop, C, 'topKNN')
+      cv2.imshow('perPixelSide', perPixelSide)
+      cv2.imshow('perPixelTop', perPixelTop)
+
 
       key = cv2.waitKey(0)
       if key == ord('s'):
         return
-      elif key == ord('d'):
+      elif key == 65288:
         print 'deleting frame %d' % curFrame
         os.remove(depthSideFiles[j])
         os.remove(depthTopFiles[j])
@@ -271,6 +313,10 @@ def main(**kwargs):
         os.remove(jointsTopFiles[j])
         os.remove(labelSideFiles[j])
         os.remove(labelTopFiles[j])
+      elif key == 65361:
+        j -= 2
+
+      j += 1
 
       #print depthSide.shape, depthTop.shape, jointsSide.shape, \
       #jointsTop.shape, labelSide.shape, labelTop.shape
@@ -279,5 +325,6 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--dir', required=True)
   parser.add_argument('--id', type=int, default=-1)
+  parser.add_argument('--start', type=int, default=0)
   args = parser.parse_args()
   main(**vars(args))
